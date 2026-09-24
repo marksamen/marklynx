ac252bb3eb2277bdc1db1bd5c3b921d06cca2399
marksamen <mark@samen.ca>
2026-09-24 10:04:34 -0400

    Add files via upload

--- copyright/js/admin-game-management_.js
+++ copyright/js/admin-game-management_.js
@@ -236,6 +236,7 @@ function openEditModal(){
 function closeEditModal(){
   gameDevFormHome.after(document.getElementById("gameDevForm"));
   gameDevActionsHome.after(document.getElementById("gameDevActions"));
+  document.getElementById("gameDevAdd").style.display="block";
   document.getElementById("gameDevEditModal").classList.remove("open");
   document.getElementById("gameDevEditModal").setAttribute("aria-hidden","true");
   document.body.style.overflow="";
